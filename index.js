092cf56edb365117f29cb45363d9c769d7e68e25
MD.Ferdous <75322918+ferdousr3@users.noreply.github.com>
2024-05-28 21:23:12 -0700

    Update index.js
    xf

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const corsOptions = {
+  origin: "https://to-do-job-task.vercel.app"
+};
+
 // app
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 //mongoDB connection
